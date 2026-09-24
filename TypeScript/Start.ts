baaca3e2d6b9ed86a16a269effd5036f05ee7a2e
tangyz <tyz198187@163.com>
2026-09-24 09:59:14 +0800

    修改编译报错

--- TypeScript/Start.ts
+++ TypeScript/Start.ts
@@ -3,7 +3,7 @@ import UE = require('ue');
 import { argv, toDelegate } from 'puerts';
 import { Define } from './Mono/Define';
 import { ToMinix } from "./AutoMixin";
-import './Test/puerts-loadue-type-compile';
+//import './Test/puerts-loadue-type-compile';
 
 Define.Game = argv.getByName("GameInstance") as UE.LyraGameInstance;
 //ToMinix(Define.Game, "AutoMixin");
